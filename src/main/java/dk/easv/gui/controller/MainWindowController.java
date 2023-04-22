52f73829e02785ca053179a6add56638e6627438
mrtng1 <mrtngrulyo@gmail.com>
2023-04-22 12:08:01 +0200

    basic login screen

--- src/main/java/dk/easv/gui/controller/MainWindowController.java
+++ src/main/java/dk/easv/gui/controller/MainWindowController.java
@@ -6,14 +6,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class MainWindowController {
     @FXML
