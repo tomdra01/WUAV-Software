a950e36e6780a36a2d830c6d319c148337fd862d
mrtng1 <mrtngrulyo@gmail.com>
2023-04-29 23:11:45 +0200

    simple password hashing

--- src/main/java/dk/easv/gui/controller/CreateUserWindowController.java
+++ src/main/java/dk/easv/gui/controller/CreateUserWindowController.java
@@ -11,6 +11,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.ResourceBundle;
 
 public class CreateUserWindowController implements Initializable {
@@ -35,8 +39,23 @@ public class CreateUserWindowController implements Initializable {
         stage.setOnCloseRequest(event -> BlurEffectUtil.removeBlurEffect(borderPane));
     }
     @FXML
-    private void submit(){
-        System.out.println(usernameField.getText()+" "+ passwordField.getText() + " " + comboBox.getValue());
+    private void submit() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        String hashedPassword = hashPassword(password);
+        String userType = (String) comboBox.getValue();
+
+        System.out.println("username: "+username + "password: " + hashedPassword + " " + userType);
+    }
+
+    private String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
     }
 
     @Override
