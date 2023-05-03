18f515dd885c2eb96c72938b1be8474776851188
mrtng1 <mrtngrulyo@gmail.com>
2023-05-03 23:48:55 +0200

    image grid pane with adding photos

--- src/main/java/dk/easv/gui/controller/project/ProjectPhotosController.java
+++ src/main/java/dk/easv/gui/controller/project/ProjectPhotosController.java
@@ -1,9 +1,16 @@
 package dk.easv.gui.controller.project;
 
 // imports
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 
 // java imports
+import java.io.File;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -14,7 +21,12 @@ import java.util.ResourceBundle;
  */
 public class ProjectPhotosController implements Initializable {
     private String projectName, businessType, projectLocation;
+    private int columnIndex = 0, rowIndex = 0;
     private LocalDate projectDate;
+    @FXML
+    private Button addBtn;
+    @FXML
+    private GridPane gridPane;
     public void setFields(String projectName, String businessType, String projectLocation, LocalDate projectDate) {
         this.projectName = projectName;
         this.businessType = businessType;
@@ -26,6 +38,32 @@ public class ProjectPhotosController implements Initializable {
                 +"\nProject location: " +projectLocation
                 +"\nProject date: " +projectDate
         );
+    }
+
+    public void addImage(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select an Image");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif")
+        );
+
+        File selectedFile = fileChooser.showOpenDialog(gridPane.getScene().getWindow());
+
+        if (selectedFile != null) {
+            Image image = new Image(selectedFile.toURI().toString());
+            ImageView imageView = new ImageView(image);
+
+            imageView.setFitWidth(100);
+            imageView.setPreserveRatio(true);
+
+            gridPane.add(imageView, columnIndex, rowIndex);
+
+            columnIndex++;
+            if (columnIndex >= gridPane.getColumnCount()) {
+                columnIndex = 0;
+                rowIndex++;
+            }
+        }
     }
 
     /**
