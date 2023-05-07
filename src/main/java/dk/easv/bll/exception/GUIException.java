387d4976539b0dd40779e15ac31eca266167dfbf
Tomáš Dračka <tomidracka@gmail.com>
2023-05-07 19:05:59 +0200

    Select MAX 3 photos of the installation

--- src/main/java/dk/easv/bll/exception/GUIException.java
+++ src/main/java/dk/easv/bll/exception/GUIException.java
@@ -8,4 +8,7 @@ public class GUIException extends RuntimeException{
     public GUIException(String message, Exception e) {
         super(message, e);
     }
+    public GUIException(String message) {
+        super(message);
+    }
 }
--- src/main/java/dk/easv/gui/controller/project/ProjectStep5Controller.java
+++ src/main/java/dk/easv/gui/controller/project/ProjectStep5Controller.java
@@ -2,6 +2,7 @@ package dk.easv.gui.controller.project;
 
 // imports
 import dk.easv.bll.exception.GUIException;
+import dk.easv.bll.util.ImageByteReader;
 import dk.easv.gui.model.ProjectModel;
 import dk.easv.gui.util.ViewType;
 import javafx.fxml.FXML;
@@ -10,13 +11,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 // java imports
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -25,9 +31,11 @@ import java.util.ResourceBundle;
  */
 public class ProjectStep5Controller implements Initializable {
     @FXML private Button nextStepBtn, previousStepBtn, addImagesBtn;
+    @FXML private ImageView imageView1, imageView2, imageView3;
     private String projectName, businessType, projectLocation, projectDescription;
     private LocalDate projectDate;
     private byte[] projectDrawing;
+    private byte[] projectImg1, projectImg2, projectImg3;
     private ProjectModel projectModel;
 
     public void setModel(ProjectModel projectModel) {
@@ -43,8 +51,54 @@ public class ProjectStep5Controller implements Initializable {
         this.projectDescription = projectDescription;
     }
 
-    public void addImages(){
+    public void addImages() throws Exception {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select images");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("image file formats", "*.jpg", "*.jpeg", "*.png"));
 
+        Stage stage = (Stage) addImagesBtn.getScene().getWindow();
+
+        List<File> selectedFiles = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            File selectedFile = fileChooser.showOpenDialog(stage);
+            if (selectedFile != null) {
+                selectedFiles.add(selectedFile);
+            } else {
+                break;
+            }
+        }
+
+        if (!selectedFiles.isEmpty()) {
+            for (int i = 0; i < selectedFiles.size(); i++) {
+                File file = selectedFiles.get(i);
+                long filesize;
+
+                Image image = new Image(file.toURI().toString());
+                switch (i) {
+                    case 0 -> {
+                        imageView1.setImage(image);
+                        projectImg1 = ImageByteReader.readImage(file);
+                        filesize = projectImg1.length;
+                        System.out.println(filesize);
+                    }
+                    case 1 -> {
+                        imageView2.setImage(image);
+                        projectImg2 = ImageByteReader.readImage(file);
+                        filesize = projectImg2.length;
+                        System.out.println(filesize);
+                    }
+                    case 2 -> {
+                        imageView3.setImage(image);
+                        projectImg3 = ImageByteReader.readImage(file);
+                        filesize = projectImg3.length;
+                        System.out.println(filesize);
+                    }
+                }
+            }
+        } else {
+            throw new GUIException("No file selected");
+        }
     }
 
     public void nextStep(){
