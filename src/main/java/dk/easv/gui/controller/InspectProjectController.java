dfaec97aa0570697ace8adfe26c82aef3484f61a
mrtng1 <mrtngrulyo@gmail.com>
2023-05-16 02:11:37 +0200

    small improvements

--- src/main/java/dk/easv/gui/controller/InspectProjectController.java
+++ src/main/java/dk/easv/gui/controller/InspectProjectController.java
@@ -1,5 +1,6 @@
 package dk.easv.gui.controller;
 
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import dk.easv.be.Project;
 import dk.easv.be.User;
@@ -8,6 +9,7 @@ import dk.easv.gui.util.ProjectDisplay;
 import dk.easv.bll.util.PopupUtil;
 import dk.easv.gui.model.ProjectModel;
 import dk.easv.gui.util.BlurEffectUtil;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,6 +25,7 @@ import java.util.ResourceBundle;
 public class InspectProjectController implements Initializable {
     @FXML private Button deleteButton, editButton;
     @FXML private JFXTextField nameField, locationField;
+    @FXML private JFXComboBox<String> businessType;
     private BorderPane borderPane;
     private ProjectModel projectModel;
     private ProjectDisplay projectDisplay;
@@ -30,6 +33,10 @@ public class InspectProjectController implements Initializable {
 
     public void setProject(Project project){
         this.project=project;
+        businessType.setItems(FXCollections.observableArrayList("Consumer", "Corporate & Government"));
+        businessType.setValue(project.getBusinessType());
+        nameField.setText(project.getName());
+        locationField.setText(project.getLocation());
     }
     public void setModel(ProjectModel projectModel){
         this.projectModel=projectModel;
@@ -63,15 +70,14 @@ public class InspectProjectController implements Initializable {
 
     public void editProject(ActionEvent actionEvent) {
         try {
-            // get the new values from the text fields
             String newName = nameField.getText();
             String newLocation = locationField.getText();
+            String newBusinessType = businessType.getValue();
 
-            // set the new values to the project object
             project.setName(newName);
             project.setLocation(newLocation);
+            project.setBusinessType(newBusinessType);
 
-            // update the project in the model (and hence in the database)
             projectModel.updateProject(project);
         } catch (DatabaseException e) {
             throw new RuntimeException(e);
